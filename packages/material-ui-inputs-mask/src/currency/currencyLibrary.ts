59a8af4874d68a77b736e81747de428ccf860960
Karafizi Artur <artur@paktolus.com>
2021-05-01 22:40:26 +0800

    Implement Currency field to inputs-mask package

---
+++ packages/material-ui-inputs-mask/src/currency/currencyLibrary.ts
@@ -0,0 +1,16 @@
+import {ICurrencyLibrary} from "./interfaces";
+
+export const currencyLibrary: ICurrencyLibrary = {
+  IDR: {
+    precision: 0,
+    symbol: "Rp",
+  },
+  USD: {
+    precision: 2,
+    symbol: "$",
+  },
+  YEN: {
+    precision: 0,
+    symbol: "¥",
+  },
+};
