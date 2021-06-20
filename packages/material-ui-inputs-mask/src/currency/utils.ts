8d7d25d2b9cdce01206fca847105be2629190046
Karafizi Artur <artur@paktolus.com>
2021-06-21 00:11:13 +0800

    fix: fix currency input (checked with 0)

--- packages/material-ui-inputs-mask/src/currency/utils.ts
+++ packages/material-ui-inputs-mask/src/currency/utils.ts
@@ -1,4 +1,4 @@
 export const getFormattedCurrency = (value: string): number => Number.parseFloat(value) * 100;
 
 export const getNormalCurrency = (value: number): string =>
-  value === void 0 ? (value !== 0 ? value / 100 : 0).toString() : "";
+  value !== void 0 ? (value !== 0 ? value / 100 : 0).toString() : "";
