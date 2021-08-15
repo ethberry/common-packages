f37244864d499b92f86eac11a1d8d52f63c8693a
TrejGun <trejgun@gmail.com>
2021-08-15 17:19:45 +0800

    test: separate nestjs and material-ui packages

--- material-ui/provider-picker/src/provider.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react";
-import AdapterDateFns from "@material-ui/lab/AdapterDateFns";
-import LocalizationProvider from "@material-ui/lab/LocalizationProvider";
-
-export const PickerProvider: FC = ({ children }) => {
-  return <LocalizationProvider dateAdapter={AdapterDateFns}>{children}</LocalizationProvider>;
-};
--- material-ui/provider-theme-ssr/src/palette.ts
+++
@@ -1,9 +0,0 @@
-import { PaletteOptions } from "@material-ui/core/styles";
-
-export const light: PaletteOptions = {
-  mode: "light",
-};
-
-export const dark: PaletteOptions = {
-  mode: "dark",
-};
