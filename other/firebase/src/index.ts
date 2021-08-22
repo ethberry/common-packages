aa4d65a7f41db4d2b89fecff59d92cb5a5099959
TrejGun <trejgun@gmail.com>
2021-08-22 15:44:30 +0800

    fix: make firebase work in storybook

--- other/firebase/src/index.ts
+++ other/firebase/src/index.ts
@@ -6,14 +6,32 @@ import "firebase/storage";
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_PROJECT_ID,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    apiKey:
+      process.env.FIREBASE_API_KEY || process.env.STORYBOOK_FIREBASE_API_KEY || process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain:
+      process.env.FIREBASE_AUTH_DOMAIN ||
+      process.env.STORYBOOK_FIREBASE_AUTH_DOMAIN ||
+      process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL:
+      process.env.FIREBASE_DB_URL || process.env.STORYBOOK_FIREBASE_DB_URL || process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId:
+      process.env.FIREBASE_PROJECT_ID ||
+      process.env.STORYBOOK_FIREBASE_PROJECT_ID ||
+      process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket:
+      process.env.FIREBASE_STORAGE_BUCKET ||
+      process.env.STORYBOOK_FIREBASE_STORAGE_BUCKET ||
+      process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:
+      process.env.FIREBASE_MESSAGE_SENDER_ID ||
+      process.env.STORYBOOK_FIREBASE_MESSAGE_SENDER_ID ||
+      process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId:
+      process.env.FIREBASE_APP_ID || process.env.STORYBOOK_FIREBASE_APP_ID || process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId:
+      process.env.FIREBASE_MEASUREMENT_ID ||
+      process.env.STORYBOOK_FIREBASE_MEASUREMENT_ID ||
+      process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
     timestampsInSnapshots: true,
   });
 }
