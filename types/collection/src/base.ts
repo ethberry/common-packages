b145a93e333058606d213cc4d82c03bca5aa7e4b
TrejGun <trejgun@gmail.com>
2021-12-20 10:29:13 +0800

    chore: id/uuid base entity interfaces

--- types/collection/src/base.ts
+++ types/collection/src/base.ts
@@ -1,5 +1,12 @@
-export interface IBase {
-  id: number;
+export interface IDateBase {
   createdAt: string;
   updatedAt: string;
+}
+
+export interface IIdBase extends IDateBase {
+  id: number;
+}
+
+export interface IUuidBase extends IDateBase {
+  uuid: string;
 }
