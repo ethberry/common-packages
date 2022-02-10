bddf00cd6a247f1f85385e9ea5b4c7d50ea1df30
TrejGun <trejgun@gmail.com>
2022-02-10 16:51:14 +0800

    fix: wallet to lower case

--- other/constants/src/eth.ts
+++ other/constants/src/eth.ts
@@ -1,2 +1,2 @@
-export const wallet = "0x87efa7f59bAA8e475F181B36f77A3028494a2cf6";
+export const wallet = "0x87efa7f59baa8e475f181b36f77a3028494a2cf6";
 export const phrase = "I am signing my one-time nonce: ";
