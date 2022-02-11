bce3a70ff0d0174bc8963d16f286aad698cd47c9
TrejGun <trejgun@gmail.com>
2022-02-11 14:51:41 +0800

    feat: split ticker from coin in coin-gecko

--- types/coin-gecko/src/coin.ts
+++ types/coin-gecko/src/coin.ts
@@ -1,3 +1,5 @@
+import { ICoinGeckoCoinTicker } from "./coin-ticker";
+
 export enum Language {
   en = "en",
   de = "de",
@@ -257,36 +259,5 @@ export interface ICoinGeckoCoin {
   };
   status_updates: Array<unknown>;
   last_updated: string;
-  tickers: Array<{
-    base: string;
-    target: string;
-    market: {
-      name: string;
-      identifier: string;
-      has_trading_incentive: boolean;
-    };
-    last: number;
-    volume: number;
-    converted_last: {
-      btc: number;
-      eth: number;
-      usd: number;
-    };
-    converted_volume: {
-      btc: number;
-      eth: number;
-      usd: number;
-    };
-    trust_score: string;
-    bid_ask_spread_percentage: number;
-    timestamp: string;
-    last_traded_at: string;
-    last_fetch_at: string;
-    is_anomaly: false;
-    is_stale: boolean;
-    trade_url: string;
-    token_info_url: null;
-    coin_id: string;
-    target_coin_id: string;
-  }>;
+  tickers: Array<ICoinGeckoCoinTicker>;
 }
