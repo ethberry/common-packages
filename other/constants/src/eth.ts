9ad60daa49cc8b8cba48d7c7b6c98535434a990a
Trej Gun <185285+TrejGun@users.noreply.github.com>
2022-03-10 15:49:25 +0800

    refactor: constants
    hardhat owner def. wallet
    add dummy tx

--- other/constants/src/eth.ts
+++ other/constants/src/eth.ts
@@ -1,2 +1,3 @@
-export const wallet = "0x87efa7f59baa8e475f181b36f77a3028494a2cf6";
+export const wallet = "0xfe3b557e8fb62b89f4916b721be55ceb828dbd73";
 export const phrase = "I am signing my one-time nonce: ";
+export const txx = "0x4278f289076cfd8d70a3e2ab416e16bad69d643319c5c9e3945a1c5ecca87692";
