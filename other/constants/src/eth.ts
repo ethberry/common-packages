bd82d735dc41950ba6e12bc345e38b3a40726cc0
TrejGun <trejgun@gmail.com>
2022-03-21 11:16:56 +0800

    chore: random fixes

--- other/constants/src/eth.ts
+++ other/constants/src/eth.ts
@@ -1,3 +1,4 @@
 export const wallet = "0xfe3b557e8fb62b89f4916b721be55ceb828dbd73";
+export const txHash = "0x4278f289076cfd8d70a3e2ab416e16bad69d643319c5c9e3945a1c5ecca87692";
+
 export const phrase = "I am signing my one-time nonce: ";
-export const txx = "0x4278f289076cfd8d70a3e2ab416e16bad69d643319c5c9e3945a1c5ecca87692";
