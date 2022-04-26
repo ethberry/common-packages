79ef0ba676388fd012b6b2b3a7895178cb7cc01b
TrejGun <trejgun@gmail.com>
2022-04-26 12:39:04 +0800

    feat: searchable interface

--- types/collection/src/search.ts
+++ types/collection/src/search.ts
@@ -3,3 +3,8 @@ import { IPaginationDto } from "./pagination";
 export interface ISearchDto extends IPaginationDto {
   query: string;
 }
+
+export interface ISearchable {
+  title: string;
+  description: string;
+}
