30ec962227ffaa2a4928da873cabd5b8e0864a05
TrejGun <trejgun@gmail.com>
2022-04-29 17:39:41 +0800

    chore: move license to portal

--- provider/provider-license/src/hook.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-
-import { LicenseContext } from "./context";
-
-export function useLicense() {
-  return useContext(LicenseContext);
-}
--- provider/provider-license/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./context";
-export * from "./provider";
-export * from "./hook";
-
-export * from "@gemunion/types-license";
