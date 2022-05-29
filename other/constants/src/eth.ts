30d666ab7f4181de8b9295dce14ef1b4962eefd4
TrejGun <trejgun@gmail.com>
2022-05-29 16:18:51 +0800

    fix: add more wallets

--- other/constants/src/eth.ts
+++ other/constants/src/eth.ts
@@ -1,4 +1,11 @@
-export const wallet = "0xfe3b557e8fb62b89f4916b721be55ceb828dbd73";
+export const wallets = [
+  "0xfe3b557e8fb62b89f4916b721be55ceb828dbd73",
+  "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
+  "0xf17f52151EbEF6C7334FAD080c5704D77216b732",
+];
+export const wallet = wallets[0];
+
 export const txHash = "0x4278f289076cfd8d70a3e2ab416e16bad69d643319c5c9e3945a1c5ecca87692";
 
 export const phrase = "I am signing my one-time nonce: ";
+export const chainId = 1337;
