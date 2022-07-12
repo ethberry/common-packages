667e11d31afccdea0a47a672eeaa6cf580d50d16
TrejGun <trejgun@gmail.com>
2022-07-12 16:58:34 +0800

    fix: drop chalk

--- other/winston-formatter/src/index.ts
+++ other/winston-formatter/src/index.ts
@@ -1,5 +1,13 @@
-import chalk from "chalk";
 import { format } from "winston";
+
+const chalk = {
+  bold: (text: string) => `\x1B[1m${text}\x1B[0m`,
+  green: (text: string) => `\x1B[32m${text}\x1B[39m`,
+  yellow: (text: string) => `\x1B[33m${text}\x1B[39m`,
+  red: (text: string) => `\x1B[31m${text}\x1B[39m`,
+  magentaBright: (text: string) => `\x1B[95m${text}\x1B[39m`,
+  cyanBright: (text: string) => `\x1B[96m${text}\x1B[39m`,
+};
 
 export const formatter = format.combine(
   format.timestamp(),
