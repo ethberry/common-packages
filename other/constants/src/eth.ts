277ca8c94d68c248c33e20feaf7fa6b88261274e
TrejGun <trejgun@gmail.com>
2023-01-28 18:03:44 +0800

    feat: add more privateKeys

--- other/constants/src/eth.ts
+++ other/constants/src/eth.ts
@@ -5,8 +5,14 @@ export const wallets = [
 ];
 export const wallet = wallets[0];
 
+export const privateKeys = [
+  "0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63",
+  "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
+  "ae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f",
+];
+export const privateKey = privateKeys[0];
+
 export const txHash = "0x4278f289076cfd8d70a3e2ab416e16bad69d643319c5c9e3945a1c5ecca87692";
-export const privateKey = "0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63";
 
 export const phrase = "I am signing my one-time nonce: ";
-export const chainId = 1337;
+export const testChainId = 13378;
