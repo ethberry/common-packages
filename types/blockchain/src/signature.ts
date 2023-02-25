867172b6cd56556034b6ee8f78b53116d290ef7d
TrejGun <trejgun@gmail.com>
2023-02-25 18:17:03 +0800

    feat: add IAsset and IParams

--- types/blockchain/src/signature.ts
+++ types/blockchain/src/signature.ts
@@ -4,3 +4,17 @@ export interface IServerSignature {
   expiresAt: number;
   bytecode?: string;
 }
+
+export interface IAsset {
+  tokenType: number;
+  token: string;
+  tokenId: string;
+  amount: string;
+}
+
+export interface IParams {
+  nonce: Uint8Array;
+  externalId: number;
+  expiresAt: number;
+  referrer: string;
+}
