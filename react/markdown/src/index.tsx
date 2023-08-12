4244218c5203eaf4d4a1edbfb3defd90c96696ef
TrejGun <trejgun@gmail.com>
2023-08-12 11:21:21 +0800

    feat!: manually upgrade marked

--- react/markdown/src/index.tsx
+++ react/markdown/src/index.tsx
@@ -1,13 +1,24 @@
 import { FC } from "react";
 import { marked } from "marked";
+import { gfmHeadingId } from "marked-gfm-heading-id";
+import { mangle } from "marked-mangle";
 import { sanitize } from "dompurify";
 
 export const COMMENT = "[//]:#";
+
+marked.use(gfmHeadingId());
+marked.use(mangle());
 
 export interface IMarkdownProps {
   text: string;
 }
 
 export const Markdown: FC<IMarkdownProps> = ({ text }) => {
-  return <div dangerouslySetInnerHTML={{ __html: sanitize(marked(text)) }} />;
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: sanitize(marked(text)),
+      }}
+    />
+  );
 };
