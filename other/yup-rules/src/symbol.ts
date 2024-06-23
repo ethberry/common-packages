43aea314f19d4ad23f984cc77022d3de8c35f75d
TrejGun <trejgun@gmail.com>
2024-06-23 17:10:20 +0800

    fix: validation message

--- other/yup-rules/src/symbol.ts
+++ other/yup-rules/src/symbol.ts
@@ -4,5 +4,5 @@ import { symbolMinLength, symbolMaxLength } from "@gemunion/constants";
 
 export const symbolValidationSchema = string()
   .required("form.validations.valueMissing")
-  .min(symbolMinLength, "form.validations.rangeUnderflow")
-  .max(symbolMaxLength, "form.validations.rangeUnderflow");
+  .min(symbolMinLength, "form.validations.tooShort")
+  .max(symbolMaxLength, "form.validations.tooLong");
--- other/yup-rules/src/title.ts
+++ other/yup-rules/src/title.ts
@@ -4,5 +4,5 @@ import { titleMinLength, titleMaxLength } from "@gemunion/constants";
 
 export const titleValidationSchema = string()
   .required("form.validations.valueMissing")
-  .min(titleMinLength, "form.validations.rangeUnderflow")
-  .max(titleMaxLength, "form.validations.rangeUnderflow");
+  .min(titleMinLength, "form.validations.tooShort")
+  .max(titleMaxLength, "form.validations.tooLong");
