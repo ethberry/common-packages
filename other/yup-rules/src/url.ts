1f3d06e7c953e5b67e700b03b6a33fdb16996e81
TrejGun <trejgun@gmail.com>
2024-07-16 21:32:01 +0100

    fix: more strict validation for imageUrl

--- other/yup-rules/src/url.ts
+++ other/yup-rules/src/url.ts
@@ -1,3 +1,3 @@
 import { string } from "yup";
 
-export const urlValidationSchema = string().required("form.validations.valueMissing");
+export const urlValidationSchema = string().required("form.validations.valueMissing").url();
