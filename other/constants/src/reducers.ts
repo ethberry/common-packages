a163aba2b47e281dc1e401743a7e541da5918c54
pavelnikolaichyk <nikolaj4ik.pasha@yandex.ru>
2024-07-23 22:22:23 +0300

    F-1 feat: added layout constants

--- other/constants/src/reducers.ts
+++ other/constants/src/reducers.ts
@@ -1,2 +1,5 @@
 export const LOCALIZATION_SLICE_NAME = "localization";
+export const LAYOUT_SLICE_NAME = "layout";
 export const SET_LANGUAGE_ACTION_TYPE = `${LOCALIZATION_SLICE_NAME}/setLanguage`;
+export const SET_LAYOUT_DIRECTION_ACTION_TYPE = `${LAYOUT_SLICE_NAME}/setLayoutDirection`;
+export const SET_LAYOUT_THEME_ACTION_TYPE = `${LAYOUT_SLICE_NAME}/setThemeType`;
