c308c256f937d7cccf991150f71dcf879746d020
pavelnikolaichyk <nikolaj4ik.pasha@yandex.ru>
2024-07-24 14:37:17 +0300

    F-1 fix: added constants connected with wallet provider

--- other/constants/src/reducers.ts
+++ other/constants/src/reducers.ts
@@ -1,5 +1,8 @@
 export const LOCALIZATION_SLICE_NAME = "localization";
 export const LAYOUT_SLICE_NAME = "layout";
+export const WALLET_SLICE_NAME = "wallet";
 export const SET_LANGUAGE_ACTION_TYPE = `${LOCALIZATION_SLICE_NAME}/setLanguage`;
 export const SET_LAYOUT_DIRECTION_ACTION_TYPE = `${LAYOUT_SLICE_NAME}/setLayoutDirection`;
 export const SET_LAYOUT_THEME_ACTION_TYPE = `${LAYOUT_SLICE_NAME}/setThemeType`;
+export const SET_ACTIVE_CONNECTOR_ACTION_TYPE = `${WALLET_SLICE_NAME}/setActiveConnector`;
+export const SET_REFERRER_ACTION_TYPE = `${WALLET_SLICE_NAME}/setReferrer`;
