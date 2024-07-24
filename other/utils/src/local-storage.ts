238e724433cf8aba7b546eec8cac310a96d92b48
pavelnikolaichyk <nikolaj4ik.pasha@yandex.ru>
2024-07-24 15:04:59 +0300

    F-1 fix: fixed local storage utils types

--- other/utils/src/local-storage.ts
+++ other/utils/src/local-storage.ts
@@ -1,4 +1,4 @@
-type LocalStorageValue = string | number | Record<string, any>;
+type LocalStorageValue = string | number | Record<string, any> | null;
 export const readFromLS = <T extends LocalStorageValue>(key: string, initialValue: T): T => {
   const data = localStorage.getItem(key);
   return data ? (JSON.parse(data) as T) : initialValue;
