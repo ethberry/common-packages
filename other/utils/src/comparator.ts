836623badb2b746b15a1590efc37b4a8c94efc50
TrejGun <trejgun@gmail.com>
2024-09-11 19:20:06 +0800

    feat: array comparator

--- other/utils/src/comparator.ts
+++ other/utils/src/comparator.ts
@@ -1,2 +1,6 @@
 export const comparator = (key: string) => (a: Record<string, any>, b: Record<string, any>) =>
   a[key] > b[key] ? 1 : -1;
+
+export const arrayComparator = <T extends Record<string, any>>(arr: Array<T>, key = "id") => {
+  return arr.slice().sort(comparator(key));
+};
