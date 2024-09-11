e8adf3e1b9745691cdb2d9b58ef4605d6bf7e62a
TrejGun <trejgun@gmail.com>
2024-09-11 19:32:46 +0800

    fix: move in utils-eth

---
+++ other/utils-eth/src/abi-encode.ts
@@ -0,0 +1,10 @@
+import { AbiCoder, keccak256, toUtf8Bytes } from "ethers";
+
+export const abiEncode = function (value: string, type: string): string {
+  const abiCoder = new AbiCoder();
+  return abiCoder.encode([type], [value]);
+};
+
+export const keccak256Utf = function (value: string): string {
+  return keccak256(toUtf8Bytes(value));
+};
---
+++ other/utils-eth/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./abi-encode";
+export * from "./block-await";
+export * from "./decode-result";
+export * from "../../../../common-packages/other/utils/src/map-series";
