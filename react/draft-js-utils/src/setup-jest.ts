0fcdb464b37160cd326e85d1fb0ab4896fedd868
TrejGun <trejgun@gmail.com>
2025-02-09 23:11:45 +0400

    fix: jsdom is a pice of crap

--- react/draft-js-utils/src/setup-jest.ts
+++ react/draft-js-utils/src/setup-jest.ts
@@ -1,5 +1,25 @@
 import utils from "util";
 
+// https://github.com/jsdom/jsdom/issues/2524
 if (typeof globalThis.TextEncoder === "undefined") {
   globalThis.TextEncoder = utils.TextEncoder;
+}
+
+// https://github.com/jsdom/jsdom/issues/2448
+if (typeof globalThis.MessageChannel === "undefined") {
+  globalThis.MessageChannel = jest.fn().mockImplementation(() => {
+    let onmessage: (cb: { data: any }) => void;
+    return {
+      port1: {
+        set onmessage(cb: (cb: { data: any }) => void) {
+          onmessage = cb;
+        },
+      },
+      port2: {
+        postMessage: (data: any) => {
+          onmessage?.({ data });
+        },
+      },
+    };
+  });
 }
